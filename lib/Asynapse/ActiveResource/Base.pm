a1dd3cfbdcca569aaf5bcd704052288776381296
Kang-min Liu <gugod@gugod.org>
2010-03-05 17:42:47 +0800

    convert the xml to hash and then to object.

--- lib/Asynapse/ActiveResource/Base.pm
+++ lib/Asynapse/ActiveResource/Base.pm
@@ -6,12 +6,13 @@ use Lingua::EN::Inflect qw(PL);
 use LWP::UserAgent;
 use URI;
 use XML::Hash;
+use Hash::AsObject;
 
 class_has 'site' => (is => "rw", isa => "Str");
 class_has 'user' => (is => "rw", isa => "Str");
 class_has 'password' => (is => "rw", isa => "Str");
 
-has 'field_attributes' => (is => "rw", isa => "HashRef");
+has '_field_attributes' => (is => "rw", isa => "HashRef");
 
 around BUILDARGS => sub {
     my $orig  = shift;
@@ -56,8 +57,9 @@ sub load_attributes_from_response {
 
     my $xc = XML::Hash->new();
     my $hash = $xc->fromXMLStringtoHash($record_xml);
-
-    $self->field_attributes($hash);
+    my ($key, $value) = each %$hash;
+    $self->_field_attributes($value);
+    return $self;
 }
 
 sub AUTOLOAD {
@@ -65,9 +67,13 @@ sub AUTOLOAD {
     local $, = ", ";
     my $self = shift;
     my @args = @_;
-    my $sub  = ${__PACKAGE__."::AUTOLOAD"};
+    my ($sub) = ${__PACKAGE__."::AUTOLOAD"} =~ /::(.+?)$/;
 
-    print STDERR "OHAI $sub: @_";
+    my $attr = $self->_field_attributes->{$sub};
+
+    return $attr if !ref $attr;
+    return $attr->{text} if $attr->{text};
+    return Hash::AsObject->new($attr);
 }
 
 
