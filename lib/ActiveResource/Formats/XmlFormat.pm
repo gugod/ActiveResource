b5825e883e2bb7d5cee28417c01ee8c0eefa9c5d
Kang-min Liu <gugod@gugod.org>
2010-03-07 16:18:19 +0800

    Implement a XmlFormat class based on XML::Hash

---
+++ lib/ActiveResource/Formats/XmlFormat.pm
@@ -0,0 +1,27 @@
+package ActiveResource::Formats::XmlFormat;
+use common::sense;
+use XML::Hash;
+
+sub extension { "xml" }
+sub mime_type { "application/xml" }
+
+sub decode {
+    my $self = shift;
+
+    my $xml = shift;
+    my $c = XML::Hash->new;
+    my $hash = $c->fromXMLStringtoHash($xml);
+    return $hash;
+}
+
+sub encode {
+    my $self = shift;
+    my $hash = shift;
+
+    my $c = XML::Hash->new;
+    my $xml = $c->fromHashtoXMLString($hash);
+    return '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . $xml;
+}
+
+
+1;
